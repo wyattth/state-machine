89c4c125e2eb908378f7f20f3e7ede8001bda995
Thomas Wyatt <thomas@Thomass-MacBook-Air.local>
2015-06-28 11:17:48 +0200

    Initial Commit

---
+++ state-machine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  state-machine
+//
+//  Created by Thomas Wyatt on 28/06/2015.
+//  Copyright (c) 2015 Thomas Wyatt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
